8e3084b8785c61e3a5a6f300175dbb881c523696
Giles <b02611037@ntu.edu.tw>
2015-08-12 11:04:37 +0800

    load file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QString>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QString filename;
+    QFileDialog ss;
+    filename = ss.getOpenFileName(this, tr("Open File"),
+                                  "/home",
+                                  tr("Images (*.png *.xpm *.jpg)"));
+
 }
 
 MainWindow::~MainWindow()
